b498d8e98c84f9a0351889f92b5754f75ac898ea
itachuchiha138 <pkpiyushkymar@gmail.com>
2025-03-05 16:25:34 +0530

    Update config.py

--- Telegram-FileStore-Bot/bot/config.py
+++ Telegram-FileStore-Bot/bot/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
+API_ID = int(os.getenv("22279653"))
+API_HASH = os.getenv("f3afedd8caf0b05be9520a210087c622")
+BOT_TOKEN = os.getenv("7748545282:AAHVrmaVNPExM3KzJvPr_sNwCgMT7PFSR24")
+MONGO_URI = os.getenv("mongodb+srv://izukumidoriya9847:<tWE5APIyCDx9yZV5>@cluster0.1dm3h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
